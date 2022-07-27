604e4bc2f463a320cd56d70db5649c1d239d5ab9
zhangling <zhanglingthai@gmail.com>
2022-07-27 12:45:09 +0700

    dev:zip

--- controller/filezip.js
+++ controller/filezip.js
@@ -2,14 +2,25 @@ const jszip = require("jszip");
 const path = require('path');
 const fs = require('fs');
 
-
-
 const FileZip = ({ files }) => {
     return new Promise((resolve, reject) => {
-        console.log(files)
-        resolve(123)
         const zip = new jszip();
-        pushZip(zip, path.resolve(__dirname, './test'));
+        console.log(typeof(files))
+
+        for (let i = 0; i < files.length; i++) {
+            
+
+            const file = files[i];
+            
+            if (file.success == true) {
+                pushZip(zip, path.resolve(__dirname, file.outputPath))
+                console.log(path.resolve(__dirname, file.outputPath))
+            } else {
+                continue;
+            }
+        }
+        // pushZip(zip, path.resolve(__dirname, './test'));
+
         zip.generateAsync({
             type: 'nodebuffer',
             compression: 'DEFLATE',
@@ -17,11 +28,16 @@ const FileZip = ({ files }) => {
                 level: 9
             }
         }).then(function (content) {
+            console.log(content)
+            
             fs.writeFile(path.resolve(__dirname, './output.zip'), content, err => {
                 if (err) throw err;
                 console.log('文件已被保存');
             });
+
+            resolve(123)
         });
+
         function pushZip(floder, pPath) {
             const files = fs.readdirSync(pPath, { withFileTypes: true });
             files.forEach((dirent, index) => {
